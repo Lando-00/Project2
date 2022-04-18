e379cef350ec94ac64cbb9158e0478c84c6c0c74
Lando-00 <lovantog@gmail.com>
2022-04-18 21:37:26 +0100

    simple change

--- Functionc.c
+++ Functionc.c
@@ -37,29 +37,6 @@ void follow(twitter *ts, user *ptr){ //takes in the whole twitter struct, and th
             currptr = currptr->nextptr;
         }
     }
-
-
-    /*
-    for (int f = 0; f < ts->num_users; f++) {
-        if (strcasecmp(ts->users[f].username, mUser) == 0)
-        {
-            //strcpy(&twitter_system->users->following[twitter_system->users->num_following],&mUser);
-            //strcpy(ts->users[f].username[ts->users[f].num_followers],mUser);
-            //ts->users[f].num_followers++;
-            //strcpy(ptr->following[ptr->num_following],ptr->username);
-            //ptr->num_following++;//priyanshi's code. I think you're copying the wrong names.
-
-            // current user follows user b
-            strcpy(&ptr->following[ptr->num_following], &ts->users[f].username );
-            // Second person's name is in following list of current user
-            ptr->num_following++; // increases following count
-            // person b has a follower
-            strcpy(&ts->users[f].followers[ts->users[f].num_followers], &ptr->username);
-            // current user's name is in follwers list of person b
-            ts->users[f].num_followers++; // increases follwers count
-        }
-    }*/
-
 }
 
 void delete_user(twitter *ts, user *curruser){//deltes current user
