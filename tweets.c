d6727ea8f621af5e27b901e147b28ca0d9974c04
Lando-00 <lovantog@gmail.com>
2022-04-29 06:46:03 +0100

    "Fixed bug of repeating tweets when more than 1 following, needed a break statement!"

--- tweets.c
+++ tweets.c
@@ -11,33 +11,35 @@
 
 void posttweet(twitter *ts, user* ptr) //function definition for function that allows users to post tweets
 {
-
+    static int iddigit = 1;
     Tweetptr currptr = malloc(sizeof(struct tweet));//creating new node for new tweet
     printf("What's on your mind?\n");
-    fgets(currptr->msg, TWEET_LENGTH,stdin);//getting input from user
+    fgets(currptr->msg, TWEET_LENGTH, stdin);//getting input from user
     fflush(stdin); //used to flush output buffer of the stream
     //adding a tweet into a node(struct) from the beginning of the list
-    if(currptr->msg[strlen(currptr->msg) - 1] == '\n') { //getting rid of the new line char
+    if (currptr->msg[strlen(currptr->msg) - 1] == '\n') { //getting rid of the new line char
         currptr->msg[strlen(currptr->msg) - 1] = '\0'; //putting a null char instead
     }
-    strcpy(currptr->user,ptr->username);
-    if(ts->tweetheadptr == NULL) //if the head pointer points to NULL
+    strcpy(currptr->user, ptr->username);
+    currptr->id = iddigit++;
+    if (ts->tweetheadptr == NULL) //if the head pointer points to NULL
     {
         // ts->newsfeed, is the head pointer for tweets in the twitter system!
-        ts->tweetheadptr=currptr; //the head pointer is the current pointer
-        currptr->nextpointer=NULL; //instead of the head pointer pointing to NULL, the current pointer now points to NULL
+        ts->tweetheadptr = currptr; //the head pointer is the current pointer
+        currptr->nextpointer = NULL; //instead of the head pointer pointing to NULL, the current pointer now points to NULL
+    } else {
+        currptr->nextpointer = ts->tweetheadptr;//make the current pointer which points to the next pointer be the head pointer (first node in the list)
+        ts->tweetheadptr = currptr;
     }
-    else
-    {
-        currptr->nextpointer=ts->tweetheadptr;//make the current pointer which points to the next pointer be the head pointer (first node in the list)
-        ts->tweetheadptr= currptr;
-    }
+
 }
 void getNewsFeed(twitter *ts, user *currUser)
 {
-    Tweetptr currptr; //temp ptr
+    Tweetptr currptr = ts->tweetheadptr; //temp ptr
+    if(currptr == NULL){
+        printf("There are no Tweets in your feed!\n");
+    }
     int tweet_check = 0;
-    currptr = ts->tweetheadptr;
     while(currptr != NULL)//going through the tweets
     {
         if(currUser->num_following > 0) // for edge cases
@@ -47,7 +49,9 @@ void getNewsFeed(twitter *ts, user *currUser)
                 if (strcasecmp(currptr->user, currUser->following[i]) == 0 ||
                     strcasecmp(currptr->user, currUser->username) == 0) {
                     tweet_check++;
-                    printf("%s\n", currptr->msg);
+                    printf("%s\n"
+                           "By User:%s, Tweet Id: %d\n", currptr->msg,currptr->user, currptr->id);
+                    break;
                 }
             }
         }
@@ -55,7 +59,8 @@ void getNewsFeed(twitter *ts, user *currUser)
         {
             if(strcasecmp(currptr->user, currUser->username) == 0){
                 tweet_check++;
-                printf("%s\n", currptr->msg);
+                printf("%s\n"
+                       "By User:%s, Tweet Id: %d\n", currptr->msg,currptr->user, currptr->id);
             }
         }
         if(tweet_check==10)
