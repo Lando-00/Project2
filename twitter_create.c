9e8177add605c0ab710c7e5ff7633f042601bd7c
Lando-00 <lovantog@gmail.com>
2022-04-30 12:59:09 +0100

    "pushed wrong twitter_create.c last time"

--- twitter_create.c
+++ twitter_create.c
@@ -12,17 +12,20 @@ void create_twitter_system(twitter * ts){
 
     //creates users first
     // using linked lists
-    Userptr currptr; //a temporary pointer which will point to the current user
+
+    Userptr currptr; //declaring a current pointer which is associated to the struct user
     ts->headptr = NULL; //initially the list is empty so head points to nothing
-    ts->tweetheadptr = NULL; //initially the list of tweets is also empty so head points to nothing
+    ts->tweetheadptr = NULL; //initially the list of tweets is empty so head points to nothing
     for(int i = 0;i < MAX_USERS;i++){
+        //newptr is the pointer which holds the address and points to the next empty node
         Userptr newptr = malloc(sizeof(user)); //allocating memory - malloc is going to return the staring address of that memory block
-        if(newptr != NULL){ //checking if space is available, i.e while newptr does not point to the end
+        if(newptr != NULL){ //space is available, i.e while newptr does not point to the end
             printf("\nEnter username of the next user or press 'tab key' then 'Enter' to exit:\n");
             fgets(newptr->username, USR_LENGHT, stdin);
-            fflush(stdin);//used to flush output buffer of the stream
-            if(newptr->username[0] == ' ') { //edge case
-                printf("(Do not start username with a 'Space'!!!)");
+            fflush(stdin);
+            if(newptr->username[0] == ' ' || newptr->username[0] == '\n') { //does not allow for empty usernames
+                printf("(Do not start username with a 'Space'!\n"
+                       "and Do not Enter Empty Usernames!)\n");
                 i--;
                 continue;
             }
@@ -34,33 +37,35 @@ void create_twitter_system(twitter * ts){
             }
             newptr->num_followers = 0;
             newptr->num_following = 0;
-            newptr->nextptr = NULL;
+            newptr->nextptr = NULL; //newptr points to the nextptr which points to nothing
         }
         if(ts->headptr == NULL)
         {
-            ts->headptr = newptr; //newptr is now the headptr and points to NULL
+            ts->headptr = newptr; //newptr is now thw headptr and points to the first node in the list
         }
         else{
             currptr = ts->headptr; //currptr is like a temporary pointer which changes what it points to everytime, unlike headptr which only points to the first node
             while(currptr->nextptr != NULL){ //while cuuptr doesn't reach the end of the list
                 currptr = currptr->nextptr; //currptr holds the address of the next node
             }
-            currptr->nextptr = newptr;
+            currptr->nextptr = newptr; //currptr (last node) holds the address of the next node which is now the newptr
         }
     }
     // prints out all of the users
     currptr = ts->headptr; //starting at the beginning of the list
     while(currptr != NULL){ //while the end of the list is not reached
-        printf("User: @%s  \t\tfollowing: %d\tfollowers: %d\n", currptr->username, currptr->num_followers, currptr->num_followers);
-        currptr = currptr->nextptr; //incrementing each time to read the next node (moving onto the next user in the list)
+        printf("User:@%20s\tFollowing:%d\t    Followers:%d\n", currptr->username, currptr->num_following, currptr->num_followers);
+        currptr = currptr->nextptr; //incrementing each time to read the next node
     }
 
+    // create another loop for when i variable reaches max.
+    // needs to re-iterate through the users and give each user the various options...
     currptr = ts->headptr; //staring at the beginning of the list
     int control;
-    while(1){ //while true
+    while(1){ //while the end of the list is not reached
         printf("\n\nCurrent User: %s\n", currptr->username);
         control = menu(ts, currptr);
-        if(control == 6){//give turn to next user
+        if(control == 6){
             if(currptr->nextptr == NULL)
             {
                 currptr = ts->headptr;
@@ -69,7 +74,7 @@ void create_twitter_system(twitter * ts){
                 }
             }
             else {
-                currptr = currptr->nextptr;
+                currptr = currptr->nextptr; //go to the next user
             }
             continue;
         }
